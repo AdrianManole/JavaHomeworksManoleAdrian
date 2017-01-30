1237cfd72afef816a29f693b11deb3657315d72e
AdrianManole <adrian_manole@ymail.com>
2017-01-30 13:23:31 +0200

    Add files via upload

---
+++ MyFirstProject.java
@@ -0,0 +1,19 @@
+/*
+ * My first project class file
+ */
+package myfirstproject;
+
+/**
+ * Class MyFirstProject is testing how Java works
+ * @author Adrian Manole
+ */
+public class MyFirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("This is my first program or not?");
+    }
+    
+}
