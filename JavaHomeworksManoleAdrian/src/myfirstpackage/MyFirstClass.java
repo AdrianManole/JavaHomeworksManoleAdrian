d8ca51e7640a166f5ee4a88fef5c5b93dfe63053
Manole Adrian <adrian_manole@ymail.com>
2017-02-04 18:50:38 +0200

    My first project

---
+++ JavaHomeworksManoleAdrian/src/myfirstpackage/MyFirstClass.java
@@ -0,0 +1,19 @@
+/*
+ * file of my first class written in Java
+ */
+package myfirstpackage;
+
+/**
+ * My first Java class
+ * @author Adrian Manole
+ */
+public class MyFirstClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is the mother of oposum!");
+    }
+    
+}
