e9aa446a6658b456cb3ca0606ee72adea781d3f8
Manole Adrian <adrian_manole@ymail.com>
2017-02-07 11:02:29 +0200

    My first self made program in Java. Wohoooo

---
+++ JavaHomeworksManoleAdrian/src/myfirstselfmadeprograminjava/MyFirstSelfMadeProgramInJava.java
@@ -0,0 +1,17 @@
+/*
+ * file of my first self made program in Java
+ */
+package myfirstselfmadeprograminjava;
+
+/**
+ * My first Java self made program
+ * @author Adrian Manole
+ */
+public class MyFirstSelfMadeProgramInJava {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Ghiță, ăsta îi primul program scris singur de mine in Java!!!");
+    }
+    
+}
